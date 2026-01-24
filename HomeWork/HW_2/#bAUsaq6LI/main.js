ceae34cbe2251aedc93710b195e23609fb3d8de2
felonvikvol <felonvikvol@gmail.com>
2026-01-24 23:22:48 +0200

    HomeWork 2 complete

---
+++ HomeWork/HW_2/#bAUsaq6LI/main.js
@@ -0,0 +1,8 @@
+let x=+prompt('Enter number:');
+
+if (x!==0){
+    console.log('Вірно');
+}
+else{
+    console.log('Невірно')
+}
---
+++ HomeWork/HW_2/#coYydZuaeEB/main.js
@@ -0,0 +1,39 @@
+let temperatures=[
+    {   day: 'Monday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    },
+    {   day: 'Tuesday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    },
+    {   day: 'Wednesday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    },
+    {   day: 'Thursday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    },
+    {   day: 'Friday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    },
+    {   day: 'Saturday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    },
+    {   day: 'Sunday',
+        morning: 8,
+        inDay: 14,
+        evening: 12
+    }
+]
+
+console.log(temperatures[1]);
